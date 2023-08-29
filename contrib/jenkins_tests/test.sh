a70166136b043bd23e1c06fe051c49da9e618355
Alexander Grissik <agrissik@nvidia.com>
2023-08-29 12:16:54 +0300

    issue: 3588301 Fix dpcp CI Test buffers
    Signed-off-by: Alexander Grissik <agrissik@nvidia.com>

--- contrib/jenkins_tests/test.sh
+++ contrib/jenkins_tests/test.sh
@@ -133,11 +133,11 @@ for test_link in $test_ip_list; do
 
 				${sudo_cmd} $timeout_exe ${vutil}  -a "${test_app}" -x "--load-vma=${test_lib} " -t "${test}:tc[1-9]$" \
 						-s "${test_remote_ip}" -p "${test_remote_port}" -l "${test_dir}/${test_name}.log" \
-						-e "XLIO_TX_BUFS=20000 XLIO_RX_BUFS=20000"
+						-e "XLIO_TX_BUFS=20000"
 			else
 				${sudo_cmd} $timeout_exe $PWD/tests/verifier/verifier.pl -a ${test_app} -x " --load-vma=$test_lib " \
 					-t ${test}:tc[1-9]$ -s ${test_ip} -l ${test_dir}/${test_name}.log \
-					-e " XLIO_TX_BUFS=20000 XLIO_RX_BUFS=20000 " \
+					-e " XLIO_TX_BUFS=20000 " \
 					--progress=0
 			fi
 
