5b77477d5db752bb87955122a8f01df4ab24f51a
Alexander Grissik <agrissik@nvidia.com>
2023-09-07 18:41:51 +0300

    issue: 3382805 Remove special routing cfg src addr field
    The m_src_addr field can be used now. No need for special one.
    
    Signed-off-by: Alexander Grissik <agrissik@nvidia.com>

--- src/core/event/netlink_event.cpp
+++ src/core/event/netlink_event.cpp
@@ -78,13 +78,12 @@ const std::string route_nl_event::to_str() const
 
     sprintf(outstr,
             "%s. ROUTE: TABLE=%u SCOPE=%u FAMILY=%u PROTOCOL=%u DST_ADDR=%s DST_PREFIX=%u "
-            "TYPE=%u PREF_SRC=%s CFG_SRC=%s IFF_NAME=%s",
+            "TYPE=%u PREF_SRC=%s IFF_NAME=%s",
             netlink_event::to_str().c_str(), p_route_val.get_table_id(), p_route_val.get_scope(),
             p_route_val.get_family(), p_route_val.get_protocol(),
             p_route_val.get_dst_addr().to_str(p_route_val.get_family()).c_str(),
             p_route_val.get_dst_pref_len(), p_route_val.get_type(),
             p_route_val.get_src_addr().to_str(p_route_val.get_family()).c_str(),
-            p_route_val.get_cfg_src_addr().to_str(p_route_val.get_family()).c_str(),
             p_route_val.get_if_name());
 
     return std::string(outstr);
