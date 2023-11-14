9ebbfeffd46a5b618721aa0cd4488e05213b3106
Alex Briskin <abriskin@nvidia.com>
2023-11-14 15:24:56 +0200

    issue: 3668182 Allow snd_buf drop below 0 in zero-copy path
    Signed-off-by: Alex Briskin <abriskin@nvidia.com>

--- src/core/lwip/tcp.h
+++ src/core/lwip/tcp.h
@@ -333,7 +333,7 @@ struct tcp_pcb {
 
     u32_t acked;
 
-    u32_t snd_buf; /* Available buffer space for sending (in bytes). */
+    s32_t snd_buf; /* Available buffer space for sending (in bytes). */
     u32_t max_snd_buff;
 
     u32_t snd_sml_snt; /* maintain state for minshall's algorithm */
@@ -480,7 +480,7 @@ err_t tcp_shutdown(struct tcp_pcb *pcb, int shut_rx, int shut_tx);
 
 err_t tcp_write(struct tcp_pcb *pcb, const void *dataptr, u32_t len, u16_t apiflags,
                 pbuf_desc *desc);
-err_t tcp_write_zc(struct tcp_pcb *pcb, const void *arg, u32_t len, pbuf_desc *desc);
+err_t tcp_write_express(struct tcp_pcb *pcb, const void *arg, u32_t len, pbuf_desc *desc);
 
 #define TCP_PRIO_MIN    1
 #define TCP_PRIO_NORMAL 64
