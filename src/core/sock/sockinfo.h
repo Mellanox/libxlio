57f813a80cc57449db1759d5ace0b6b5bb43ac45
Alexander Grissik <agrissik@nvidia.com>
2024-02-12 11:27:40 +0200

    issue: 3777348 Removing unused pkt_sndr_source class
    The class pkt_sndr_source is unused empty interface.
    This adds undesired vptr to inheriting classes and trashes the cache.
    
    Signed-off-by: Alexander Grissik <agrissik@nvidia.com>

--- src/core/sock/sockinfo.h
+++ src/core/sock/sockinfo.h
@@ -52,7 +52,6 @@
 
 #include "socket_fd_api.h"
 #include "pkt_rcvr_sink.h"
-#include "pkt_sndr_source.h"
 #include "sock-redirect.h"
 #include "sock-app.h"
 
@@ -155,10 +154,7 @@ typedef std::unordered_map<ring *, ring_info_t *> rx_ring_map_t;
 // see route.c in Linux kernel
 const uint8_t ip_tos2prio[16] = {0, 0, 0, 0, 2, 2, 2, 2, 6, 6, 6, 6, 4, 4, 4, 4};
 
-class sockinfo : public socket_fd_api,
-                 public pkt_rcvr_sink,
-                 public pkt_sndr_source,
-                 public wakeup_pipe {
+class sockinfo : public socket_fd_api, public pkt_rcvr_sink, public wakeup_pipe {
 public:
     sockinfo(int fd, int domain, bool use_ring_locks);
     ~sockinfo() override;
