152736d2fed8c9cc2ea00bc52940bec7a867e616
Alex Briskin <abriskin@nvidia.com>
2024-03-26 11:24:47 +0200

    issue: 3782717 tx_fifo_scheduler + tests
    Signed-off-by: Alex Briskin <abriskin@nvidia.com>

--- src/core/fairness/tx_scheduler.h
+++ src/core/fairness/tx_scheduler.h
@@ -32,10 +32,78 @@
 #ifndef _TX_SCHEDULER_H_
 #define _TX_SCHEDULER_H_
 
-class ring;
 class tcp_segment;
 class udp_datagram;
 class control_msg;
+class sockinfo;
+
+class ring {
+public:
+    virtual ~ring() = default;
+
+    virtual void notify_complete(uintptr_t) = 0;
+    virtual bool send(tcp_segment &, uintptr_t) = 0;
+    virtual bool send(udp_datagram &, uintptr_t) = 0;
+    virtual bool send(control_msg &, uintptr_t) = 0;
+};
+
+class tx_scheduler {
+public:
+    tx_scheduler(ring &r, size_t max_requests)
+        : m_ring(r)
+        , m_max_requests(max_requests)
+        , m_num_requests(0UL)
+    {
+    }
+
+    ~tx_scheduler() = default;
+
+    virtual void notify_ready_to_send(sockinfo *, bool = true) = 0;
+    virtual void notify_completion(uintptr_t, size_t = 1) = 0;
+    virtual void schedule_tx() = 0;
+
+    enum status { OK, NO_MORE_MESSAGES, ERROR };
+
+    template <class Msg> inline bool send(Msg &msg, uintptr_t metadata)
+    {
+        if (m_num_requests < m_max_requests) {
+            m_num_requests++;
+            m_ring.send(msg, metadata);
+            return true;
+        }
+        return false;
+    }
+
+protected:
+    ring &m_ring;
+    size_t m_max_requests;
+    size_t m_num_requests;
+};
+
+class sq_proxy final {
+public:
+    sq_proxy(tx_scheduler &sched, size_t num_messages, uintptr_t metadata)
+        : m_scheduler(sched)
+        , m_num_messages(num_messages)
+        , m_metadata(metadata)
+    {
+    }
+    ~sq_proxy() = default;
+
+    template <class Msg> inline bool send(Msg &msg)
+    {
+        if (m_num_messages) {
+            m_num_messages--;
+            return m_scheduler.send(msg, m_metadata);
+        }
+        return false;
+    }
+
+private:
+    tx_scheduler &m_scheduler;
+    size_t m_num_messages;
+    uintptr_t m_metadata;
+};
 
 class sockinfo {
 public:
@@ -43,46 +111,44 @@ public:
     virtual ~sockinfo() = default;
 
     virtual void notify_completion(size_t num_completions = 1) = 0;
-    virtual bool do_send(ring *) = 0;
+    virtual tx_scheduler::status do_send(sq_proxy sq) = 0;
 };
 
-class ring {
-public:
-    virtual ~ring() = default;
-
-    virtual void notify_complete(uintptr_t) = 0;
-    virtual bool send(tcp_segment *, sockinfo *) = 0;
-    virtual bool send(udp_datagram *, sockinfo *) = 0;
-    virtual bool send(control_msg *, sockinfo *) = 0;
-};
-
-class tx_scheduler {
-public:
-    tx_scheduler() = default;
-    ~tx_scheduler() = default;
-
-    virtual void notify_ready_to_send(sockinfo *, bool) = 0;
-    virtual void notify_completion(sockinfo *, size_t num_completions = 0) = 0;
-    virtual void fair_send() = 0;
-};
-
-/* TODO In future migrate to tx_fifo_scheduler.h */
-#include <iostream>
-#include <cassert>
-
+#include <map>
 class tx_fifo_scheduler final : public tx_scheduler {
 public:
-    tx_fifo_scheduler(ring *r)
-        : m_ring(r) {};
+    tx_fifo_scheduler(ring &r, size_t max_requests)
+        : tx_scheduler(r, max_requests)
+    {
+    }
+
     ~tx_fifo_scheduler() = default;
 
-    void notify_ready_to_send(sockinfo *, bool) override { }
+    void notify_ready_to_send(sockinfo *sock, bool) override
+    {
+        sock->do_send({*this, m_max_requests - m_num_requests, reinterpret_cast<uintptr_t>(sock)});
+    }
 
-    void notify_completion(sockinfo *, size_t num_completions = 0) override { }
+    void notify_completion(uintptr_t metadata, size_t num_completions = 1) override
+    {
+        sockinfo *socket = reinterpret_cast<sockinfo *>(metadata);
+        m_notifications[socket] += num_completions;
+    }
 
-    void fair_send() override { }
+    void schedule_tx() override
+    {
+        /* The sockets are not allowed to destruct before all notifications received */
+        for (auto tx_notify : m_notifications) {
+            tx_notify.first->notify_completion(tx_notify.second);
+        }
+        m_notifications.clear();
+    }
 
 private:
+    std::map<sockinfo *, size_t> m_notifications;
     ring *m_ring;
+    size_t m_max_requests;
+    size_t m_num_requests;
 };
+
 #endif // _TX_SCHEDULER_H_
--- src/core/fairness/tx_scheduler_test.cpp
+++ src/core/fairness/tx_scheduler_test.cpp
@@ -35,29 +35,89 @@
 #include <gmock/gmock.h>
 #include "tx_scheduler.h"
 
+using ::testing::_; // Matcher for any argument
+using ::testing::Return;
+
 /* Mock classes created for testing tx_scheduler */
 class sockinfo_mock : public sockinfo {
 public:
-    MOCK_METHOD(void, notify_completion, (size_t num_cpmpletions), (override));
-    MOCK_METHOD(bool, do_send, (ring *), (override));
+    MOCK_METHOD(void, notify_completion, (size_t), (override));
+    MOCK_METHOD(tx_scheduler::status, do_send, (sq_proxy), (override));
 };
 
 class ring_mock : public ring {
 public:
-
     ring_mock() = default;
-	~ring_mock() override = default;
+    ~ring_mock() override = default;
 
     MOCK_METHOD(void, notify_complete, (uintptr_t), (override));
 
     // Mock the send methods
-    MOCK_METHOD(bool, send, (tcp_segment *, sockinfo *), (override));
-    MOCK_METHOD(bool, send, (udp_datagram *, sockinfo *), (override));
-    MOCK_METHOD(bool, send, (control_msg *, sockinfo *), (override));
+    MOCK_METHOD(bool, send, (tcp_segment &, uintptr_t), (override));
+    MOCK_METHOD(bool, send, (udp_datagram &, uintptr_t), (override));
+    MOCK_METHOD(bool, send, (control_msg &, uintptr_t), (override));
 };
 
-TEST(tx_scheduler, constructor)
+TEST(tx_fifo_scheduler, send_max_num_requests)
 {
-    ring_mock r{};
-    tx_fifo_scheduler fifo(&r);
+    ring_mock ring {};
+    sockinfo_mock socket {};
+    size_t max_iflight_requests = 10;
+    tx_fifo_scheduler fifo(ring, max_iflight_requests);
+
+    EXPECT_CALL(socket, do_send(_))
+        .Times(1)
+        .WillOnce(Return(tx_scheduler::status::OK)); // Always return status_OK
+
+    fifo.notify_ready_to_send(&socket, true);
+}
+
+TEST(tx_fifo_scheduler, fifo_does_no_do_send_in_schedule_tx)
+{
+    ring_mock ring {};
+    sockinfo_mock socket {};
+    size_t max_iflight_requests = 10;
+    tx_fifo_scheduler fifo(ring, max_iflight_requests);
+
+    EXPECT_CALL(socket, do_send(_)).Times(0);
+
+    fifo.schedule_tx();
+}
+
+TEST(tx_fifo_scheduler, fifo_notifies_sockets_in_schedule_tx)
+{
+    ring_mock ring {};
+    sockinfo_mock socket {};
+    size_t max_iflight_requests = 10;
+    tx_fifo_scheduler fifo(ring, max_iflight_requests);
+
+    fifo.notify_completion(reinterpret_cast<uintptr_t>(&socket), 42);
+    EXPECT_CALL(socket, notify_completion(42)).Times(1);
+
+    fifo.schedule_tx();
+}
+
+TEST(tx_fifo_scheduler, fifo_notifies_no_sockets_when_completions_empty_in_schedule_tx)
+{
+    ring_mock ring {};
+    sockinfo_mock socket {};
+    size_t max_iflight_requests = 10;
+    tx_fifo_scheduler fifo(ring, max_iflight_requests);
+
+    EXPECT_CALL(socket, notify_completion(_)).Times(0);
+    fifo.schedule_tx();
+}
+
+TEST(tx_fifo_scheduler, fifo_notifies_once_per_sockets_in_schedule_tx)
+{
+    ring_mock ring {};
+    sockinfo_mock socket {};
+    size_t max_iflight_requests = 10;
+    tx_fifo_scheduler fifo(ring, max_iflight_requests);
+
+    fifo.notify_completion(reinterpret_cast<uintptr_t>(&socket), 22);
+    fifo.notify_completion(reinterpret_cast<uintptr_t>(&socket), 20);
+    EXPECT_CALL(socket, notify_completion(42)).Times(1);
+
+    fifo.schedule_tx();
 }
