564f27b992b5a0051fd13bb7e8bd061afb1a846b
Dmytro Podgornyi <dmytrop@nvidia.com>
2024-03-29 02:16:47 +0200

    issue: 3928453 XLIO_PRINT_REPORT prints report unconditionally
    Signed-off-by: Dmytro Podgornyi <dmytrop@nvidia.com>

--- src/core/dev/buffer_pool.h
+++ src/core/dev/buffer_pool.h
@@ -79,7 +79,7 @@ public:
     void register_memory();
     void print_val_tbl();
     void print_report(vlog_levels_t log_level = VLOG_DEBUG);
-    static void print_report_on_errors(vlog_levels_t log_level);
+    static void print_full_report(vlog_levels_t log_level);
 
     uint32_t find_lkey_by_ib_ctx_thread_safe(ib_ctx_handler *p_ib_ctx_h);
     doca_mmap *get_doca_mmap() const { return m_allocator_data.get_doca_mmap(); }
