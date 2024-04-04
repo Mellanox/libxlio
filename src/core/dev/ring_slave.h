da4186b3e895674de20c3a5eca8f85be3a29a4df
Alexander Grissik <agrissik@nvidia.com>
2024-04-04 10:11:21 +0300

    issue: 3598943 Fixing adaptive CQ moderation
    1. Removing hardcoded check that switches AIM to latency mode.
    In case of low packet rate the calculation will result in 0 count anyway.
    In case packet rate is higher than the desired interrupt rate we do want to utilize the AIM correctly.
    2. Changing default AIM values to more reasonable.
    3. Removing default values for Nginx and use AIM by default.
    This improves CPU utilization in low congested cases significantly.:
    
    Signed-off-by: Alexander Grissik <agrissik@nvidia.com>

--- src/core/dev/ring_slave.h
+++ src/core/dev/ring_slave.h
@@ -290,7 +290,7 @@ public:
     virtual void inc_tx_retransmissions_stats(ring_user_id_t id);
     bool rx_process_buffer(mem_buf_desc_t *p_rx_wc_buf_desc, void *pv_fd_ready_array);
     virtual int reclaim_recv_single_buffer(mem_buf_desc_t *rx_reuse) = 0;
-    virtual void inc_cq_moderation_stats(size_t sz_data) = 0;
+    virtual void inc_cq_moderation_stats() = 0;
 
     virtual bool attach_flow(flow_tuple &flow_spec_5t, sockinfo *sink, bool force_5t = false);
     virtual bool detach_flow(flow_tuple &flow_spec_5t, sockinfo *sink);
