3cc29571b2ea0b7677a49bb8fdf3f2ce9398b85f
Iftah Levi <iftahl@nvidia.com>
2024-05-29 13:57:33 +0000

    issue: Fix CPU affinity set by dpdk in doca_flow_init
    Signed-off-by: Iftah Levi <iftahl@nvidia.com>

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -1030,6 +1030,8 @@ static doca_error_t init_doca_flow()
 {
     doca_error_t result, tmp_result;
     struct doca_flow_cfg *rxq_flow_cfg;
+    cpu_set_t mask;
+    pid_t pid;
 
 #if defined(DEFINED_NGINX)
     // Skip DOCA Flow initialization for Nginx master process.
@@ -1057,11 +1059,14 @@ static doca_error_t init_doca_flow()
         goto destroy_cfg;
     }
 
+    pid = getpid();
+    sched_getaffinity(pid, sizeof(cpu_set_t), &mask);
     result = doca_flow_init(rxq_flow_cfg);
     if (result != DOCA_SUCCESS) {
         VPRINT_DOCA_ERR(VLOG_ERROR, result, "doca_flow_init\n");
         goto destroy_cfg;
     }
+    sched_setaffinity(pid, sizeof(cpu_set_t), &mask);
 
 destroy_cfg:
     tmp_result = doca_flow_cfg_destroy(rxq_flow_cfg);
