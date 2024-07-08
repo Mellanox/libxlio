9197c8a38f24a8ece9d939d1e3301b845e60687c
Viacheslav Login <vlogin@nvidia.com>
2024-07-08 09:21:36 +0300

    [CI] DOCA compilation: fix compiler.sh failures
    We're running compiler.sh in Ubuntu22.04, where there is a problem using module files, created for RHEL/CentOS
    For now, to make things working we install all the required compilers inside container:
    
    - Install all required compilers inside comtainer rather than connecting modules
    - Use update-alternatives to switch between compilers
    - Update matrix_job.yaml with toolbox target for docker build
    
    Issue: HPCINFRA-1968
    Signed-off-by: Viacheslav Login <vlogin@nvidia.com>

--- contrib/jenkins_tests/compiler.sh
+++ contrib/jenkins_tests/compiler.sh
@@ -10,24 +10,23 @@ rm -rf $compiler_dir
 mkdir -p $compiler_dir
 cd $compiler_dir
 
-compiler_list="clang:clang++:dev/clang-9.0.1 icc:icpc:intel/ics-18.0.4 icc:icpc:intel/ics-19.1.1 gcc:g++:dev/gcc-8.3.0 gcc:g++:dev/gcc-9.3.0 gcc:g++:dev/gcc-10.1.0"
+compiler_list="clang:clang++:clang-15 gcc:g++:gcc-8 gcc:g++:gcc-9 gcc:g++:gcc-10 gcc:g++:gcc-11"
 
 compiler_tap=${WORKSPACE}/${prefix}/compiler.tap
 echo "1..$(echo $compiler_list | tr " " "\n" | wc -l)" > $compiler_tap
 
 test_id=0
 for compiler in $compiler_list; do
-    IFS=':' read cc cxx module <<< "$compiler"
+    IFS=':' read cc cxx version <<< "$compiler"
+    update-alternatives --set $cc /usr/bin/$version
     mkdir -p ${compiler_dir}/${test_id}
     cd ${compiler_dir}/${test_id}
-    [ -z "$module" ] && test_name=$($cc --version | head -n 1) || test_name="$module"
-    [ ! -z "$module" ] && do_module "$module"
+    test_name="$version"
     echo "======================================================"
     $cc --version
     echo
     test_exec='${WORKSPACE}/configure --prefix=$compiler_dir-$cc CC=$cc CXX=$cxx --disable-lto $jenkins_test_custom_configure && make $make_opt all'
     do_check_result "$test_exec" "$test_id" "$test_name" "$compiler_tap" "${compiler_dir}/compiler-${test_id}"
-    [ ! -z "$module" ] && module unload "$module"
     cd ${compiler_dir}
     test_id=$((test_id+1))
 done
