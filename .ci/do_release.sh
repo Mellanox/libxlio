bc0901bbb9446c102cbdc21642df191ac9c7de0f
NirWolfer <nwolfer@nvidia.com>
2024-07-17 16:04:19 +0300

    issue: HPCINFRA-776 [CI] Release pipeline
    Unti now, release process is done manually by the dev team. This process
    involves creating a tag on a commit that needs to be released, building
    that tag and copying the files to NFS location
    
    The idea is to create a jenkins pipeline that will do the entire process
    with given inputs, This commit will commence phase 1 - doing the build
    and copying of packages created to NFS.
    THE CREATION OF A RELEASE TAG ON A COMMIT IS NOT PART OF THIS PR
    
    Add release_jjb and the corosponding matrix file for a new pipeline
    definition, the release is run on rhel8.6
    Add ARG HARBOR_URL to dockerfile, to support DRP in the future since the
    release process needs to run there as well
    Change do_release.sh script to match the new parameters provided by the
    jenkins job and pass ShellCheck
    Change create_pkg.sh to pass ShellCheck
    
    Signed-off-by: NirWolfer <nwolfer@nvidia.com>

--- .ci/do_release.sh
+++ .ci/do_release.sh
@@ -1,77 +1,85 @@
 #!/bin/bash -Exel
 
-echo -e "\n\n**********************************"
-echo -e "\n\nStarting do_release.sh script...\n\n"
-echo -e "**********************************\n\n"
+echo "**********************************"
+echo "Starting do_release.sh script..."
+echo "**********************************"
 
-if [ -z "$1" ]; then
-    if [ -z "${release_folder}" ]; then
-        echo "ERROR: Please use the first script argument or env var 'release_folder'. Exit"
-    fi
-else
-    release_folder=$1
-fi
-if [ ! -e "${release_folder}" ] || [ ! -d "${release_folder}" ]; then
-    echo "ERROR: [${release_folder}] directory doesn't exist. Exit"
+set -o pipefail
+
+print_help() {
+    set +xv  
+    echo -e "\n\n"
+    echo "--------------------------------------------------"
+    echo "Usage: release_folder=<release folder> release_tag=<release tag> [revision=<revision>] [do_release=<true|false>] $0"
+    echo "       Where release folder is a path to NFS folder to copy the package into"
+    echo "       Where release tag is a git tag to release (must be already tagged in the git repo)"
+    echo "       Where revision is a number postfix to add the to package indicating which version of the tag it is - OPTIONAL, default value 1"
+    echo "       Where do_release is a boolean value indicating if the script will copy the created package into the release folder location - OPTIONAL, default value false"
     exit 1
+}
+
+if [ -z "${release_folder}" ]; then
+    echo "ERROR: 'release_folder' was not set."
+    print_help
 fi
 
-if [ -z "$2" ]; then
-    if [ -z "${release_version}" ]; then
-        echo "ERROR: Please use the second script argument or env var 'release_version'. Exit"
-    fi
-else
-    release_version=$2
-    echo "FULL_VERSION from script parameter: [${release_version}]"
+if [ ! -e "${release_folder}" ] || [ ! -d "${release_folder}" ]; then
+    echo "ERROR: [${release_folder}] directory doesn't exist."
+    print_help
 fi
 
-env PRJ_RELEASE=1 contrib/build_pkg.sh -s
+if [ -z "${release_tag}" ]; then
+    echo "ERROR: 'release_tag' was not set."
+    print_help
+fi
 
-MAJOR_VERSION=$(cat configure.ac | grep -e "define(\[prj_ver_major\]" | awk -e '{ printf $2 };' | sed  's/)//g')
-MINOR_VERSION=$(cat configure.ac | grep -e "define(\[prj_ver_minor\]" | awk -e '{ printf $2 };' | sed  's/)//g')
-REVISION_VERSION=$(cat configure.ac | grep -e "define(\[prj_ver_revision\]" | awk -e '{ printf $2 };' | sed  's/)//g')
+if [ -z "${revision}" ]; then
+    echo "WARN: 'revision' was not set, defaulting to 1"
+    revision=1
+fi
+
+if [ -z "${do_release}" ]; then
+    echo "WARN: 'do_release' was not set, defaulting to false (package will not be release)"
+    do_release=false
+fi
+
+env PRJ_RELEASE="${revision}" contrib/build_pkg.sh -s
+
+MAJOR_VERSION=$(grep -e "define(\[prj_ver_major\]" configure.ac | awk '{ printf $2 };' | sed  's/)//g')
+MINOR_VERSION=$(grep -e "define(\[prj_ver_minor\]" configure.ac | awk '{ printf $2 };' | sed  's/)//g')
+REVISION_VERSION=$(grep -e "define(\[prj_ver_revision\]" configure.ac | awk '{ printf $2 };' | sed  's/)//g')
 configure_ac_version="${MAJOR_VERSION}.${MINOR_VERSION}.${REVISION_VERSION}"
 echo "FULL_VERSION from configure.ac: [${configure_ac_version}]"
 
-last_tag=$(git describe --tags $(git rev-list --tags --max-count=1))
-echo "Last tag: [${last_tag}]"
-
-if [[ "$last_tag" != "${configure_ac_version}" ]]; then
-    echo "ERROR: FULL_VERSION from configure.ac doesn't match last tag version! Exit"
+if [[ "${release_tag}" != "${configure_ac_version}" ]]; then
+    echo "ERROR: FULL_VERSION: ${configure_ac_version} from configure.ac doesn't match tag: ${release_tag} provided! Exit"
     exit 1
 fi
 
-if [ -z "${release_version}" ]; then
-    release_version=${configure_ac_version}
-else
-    if [[ "$last_tag" != "${release_version}" ]]; then
-        echo "ERROR: FULL_VERSION from script parameter doesn't match last tag version! Exit"
+if [ "${do_release}" = true ] ; then
+    echo "do_release is set to true, will release package into ${release_folder}/${release_tag}"
+
+    cd pkg/packages || { echo "pkg folder is missing, exiting..."; exit 1; }
+    pkg_name=$(ls -1 libxlio-"${release_tag}"-"${revision}".src.rpm)
+    DST_DIR=${release_folder}/${release_tag}
+
+    if [[ -e "${DST_DIR}/${pkg_name}" ]]; then 
+        echo "ERROR: [${DST_DIR}/${pkg_name}] file already exist. Exit"
         exit 1
     fi
+
+    sudo -E -u swx-jenkins mkdir -p "$DST_DIR"
+    sudo -E -u swx-jenkins cp -v "${pkg_name}" "$DST_DIR"
+
+    cd "${release_folder}"
+    sudo -E -u swx-jenkins ln -s "$DST_DIR/${pkg_name}" "${pkg_name}"
+    sudo -E -u swx-jenkins echo "${pkg_name}" | sudo -E -u swx-jenkins tee "${DST_DIR}"/../latest.txt
+    echo "Release found at $DST_DIR"
+else
+     echo "do_release is set to false, skipping package release."
 fi
 
-_UID=6213
-_GID=101
-_LOGIN=swx-jenkins
-_HOME=/var/home/$_LOGIN
-echo "${_LOGIN} ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-echo "root ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-groupadd -f -g "$_GID" "$_LOGIN"
-useradd -u "$_UID" -g "$_GID" -s /bin/bash -m -d ${_HOME} "${_LOGIN}"
-
-cd $WORKSPACE/pkg/packages
-
-pkg_name=$(ls -1 libxlio-*.src.rpm)
-
-DST_DIR=${release_folder}/${release_version}
-sudo -E -u swx-jenkins sh -c "mkdir -p $DST_DIR"
-
-if [[ -e "${DST_DIR}/${pkg_name}" ]]; then 
-    echo "ERROR: [${DST_DIR}/${pkg_name}] file already exist. Exit"
-    exit 1
-fi
-
-sudo -E -u swx-jenkins sh -c "cp -v ${pkg_name} $DST_DIR"
-
-cd ${release_folder}
-sudo -E -u swx-jenkins sh -c "ln -s $DST_DIR/${pkg_name} ${pkg_name}"
+set +x
+echo "**********************************"
+echo "Finished do_release.sh script..."
+echo "**********************************"
