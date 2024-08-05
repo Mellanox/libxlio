iftahl/libxlio-pub

issue: 3898040 DOCA RX data path with hw_queue_rx
Adding RX data-path with DOCA ETH RX.
Utilizes DOCA regular RX queue.
DOCA path is completely moved to hw_queue_rx without using cq_mgr_rx.
DPCP path is mostly unchanged and is implemented with cq_mgr_rx.

Signed-off-by: Alexander Grissik <agrissik@nvidia.com>

src/core/main.cpp
@@ -822,6 +822,8 @@ void print_xlio_global_settings()
                           option_3::to_str(safe_mce_sys().enable_lro));
     VLOG_PARAM_STRING("BF (Blue Flame)", safe_mce_sys().handle_bf, MCE_DEFAULT_BF_FLAG, SYS_VAR_BF,
                       safe_mce_sys().handle_bf ? "Enabled " : "Disabled");
+    VLOG_PARAM_STRING("DOCA FLOW", safe_mce_sys().doca_flow, false, "XLIO_DOCA_FLOW",
+                      safe_mce_sys().doca_flow ? "Enabled " : "Disabled");
 #ifdef DEFINED_UTLS
     VLOG_PARAM_STRING("UTLS RX support", safe_mce_sys().enable_utls_rx, MCE_DEFAULT_UTLS_RX,
                       SYS_VAR_UTLS_RX, safe_mce_sys().enable_utls_rx ? "Enabled " : "Disabled");

Mellanox/libxlio

src/core/main.cpp
@@ -820,6 +820,8 @@ void print_xlio_global_settings()
     VLOG_STR_PARAM_STRING("LRO support", option_3::to_str(safe_mce_sys().enable_lro),
                           option_3::to_str(MCE_DEFAULT_LRO), SYS_VAR_LRO,
                           option_3::to_str(safe_mce_sys().enable_lro));
+    VLOG_PARAM_STRING("DOCA RX", safe_mce_sys().doca_rx, false, "XLIO_DOCA_RX",
+                      safe_mce_sys().doca_rx ? "Enabled " : "Disabled");
 #ifdef DEFINED_UTLS
     VLOG_PARAM_STRING("UTLS RX support", safe_mce_sys().enable_utls_rx, MCE_DEFAULT_UTLS_RX,
                       SYS_VAR_UTLS_RX, safe_mce_sys().enable_utls_rx ? "Enabled " : "Disabled");