0cf78071e9554355e2a9a186103d959a420f71bc
Alexander Grissik <agrissik@nvidia.com>
2024-08-13 09:28:43 +0000

    issue: 4033654 Cleanup of rotten tests
    Delete tests that are not used for years and without any intention to be reused.
    
    Signed-off-by: Alexander Grissik <agrissik@nvidia.com>

--- contrib/xlio-bench/lib/wrk-json.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# bash unofficial strict mode:
-set -euo pipefail
-IFS=$'\n\t'
-
-DIR="$(dirname "$0")"
-export LUA_PATH="$DIR/?.lua"
-ARCH_NAME="$(uname -m | sed 's/_.*//')"
-"$DIR/../env/default-$ARCH_NAME/bin/wrk" -s "$DIR/json_report.lua" "$@" | grep json_report
