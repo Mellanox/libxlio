16c70fd86cb21c5aac77cc6e5a384f2c4e9e138f
Tomer Cabouly <tcabouly@nvidia.com>
2024-12-25 09:55:15 +0000

    WIP

--- contrib/jenkins_tests/test.sh
+++ contrib/jenkins_tests/test.sh
@@ -130,9 +130,12 @@ for test_link in $test_ip_list; do
 				${sudo_cmd} $timeout_exe ${vutil}  -a "${test_app}" -x "--load-vma=${test_lib} " -t "${test}:tc[1-9]$" \
 						-s "${test_remote_ip}" -p "${test_remote_port}" -l "${test_dir}/${test_name}.log"
 			else
+				pushd $PWD
+				wget https://github.com/user-attachments/files/18245148/doca_support.patch
+				git apply doca_support.patch
 				${sudo_cmd} $timeout_exe $PWD/tests/verifier/verifier.pl -a ${test_app} -x " --pre-warmup-wait=2 --debug " \
 					-t ${test}:tc[6-9]$ -s ${test_ip} -l ${test_dir}/${test_name}.log \
-					-e " XLIO_MEM_ALLOC_TYPE=ANON XLIO_DOCA_RX=0 XLIO_DOCA_TX=0 LD_PRELOAD=$test_lib " \
+					-e " XLIO_MEM_ALLOC_TYPE=ANON XLIO_DOCA_RX=1 XLIO_DOCA_TX=1 LD_PRELOAD=$test_lib " \
 					--progress=0
 			fi
 
