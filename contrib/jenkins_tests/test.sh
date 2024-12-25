9a40ec4d6f0c9d1b8c50f20d07ada2bd7f39f13d
Tomer Cabouly <tcabouly@nvidia.com>
2024-12-25 17:45:52 +0200

    WIP@

--- contrib/jenkins_tests/test.sh
+++ contrib/jenkins_tests/test.sh
@@ -133,8 +133,9 @@ for test_link in $test_ip_list; do
 				pushd $PWD
 				rm -f doca_support*
 				wget https://github.com/user-attachments/files/18245687/doca_support.patch
-				git apply doca_support.patch
+				patch -p1 < doca_support.patch
 				rm -f doca_support.patch
+				popd
 				${sudo_cmd} $timeout_exe $PWD/tests/verifier/verifier.pl -o 4 -a ${test_app} -x " --pre-warmup-wait=2 --debug " \
 					-t ${test}:tc[6-9]$ -s ${test_ip} -l ${test_dir}/${test_name}.log \
 					-e " XLIO_MEM_ALLOC_TYPE=ANON XLIO_DOCA_RX=1 XLIO_DOCA_TX=1 LD_PRELOAD=$test_lib " \
