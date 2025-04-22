Mellanox/libxlio

commit c73d96a3e7a9b64c77ed024fa94cf00b7fdca339

issue: 3777348 Simplifying timers for TCP sockets
Removing inheritance of timer_handler from sockinfo_tcp allows o order fields in cache friendly manner.
timer_hander add vptr ad related fields in an undesired location in the object layout.
sockinfo_tcp is always handled by tcp_timer_collection which itslef is timer_handler.
Thus, there is no real need in registering the socket itself into the internal thread.
This removes complex code with handling groups inside timers registration which are used only for tcp_collection.
For XLIO_TCP_CTL_THREAD=with_wakeup/no_wakeup we put the listen socket events to be handled as part of tcp_timer_collections
instead of being registered directly on internal thread.

Signed-off-by: Alexander Grissik <agrissik@nvidia.com>

src/core/event/event_handler_manager.cpp
@@ -42,7 +42,7 @@
 #include "timer_handler.h"
 #include "event_handler_ibverbs.h"
 #include "event_handler_rdma_cm.h"
-
+#include "core/sock/sockinfo_tcp.h"
 #include "core/util/instrumentation.h"
 
 #define MODULE_NAME "evh:"
@@ -86,8 +86,7 @@ event_handler_manager *g_p_event_handler_manager = nullptr;
 pthread_t g_n_internal_thread_id = 0;
 
 void *event_handler_manager::register_timer_event(int timeout_msec, timer_handler *handler,
-                                                  timer_req_type_t req_type, void *user_data,
-                                                  timers_group *group /* = NULL */)
+                                                  timer_req_type_t req_type, void *user_data)
 {
     evh_logdbg("timer handler '%p' registered %s timer for %d msec (user data: %p)", handler,
                timer_req_type_str(req_type), timeout_msec, user_data);
@@ -115,12 +114,21 @@ void *event_handler_manager::register_timer_event(int timeout_msec, timer_handle
     reg_action.type = REGISTER_TIMER;
     reg_action.info.timer.handler = handler;
     reg_action.info.timer.user_data = user_data;
-    reg_action.info.timer.group = group;
     reg_action.info.timer.node = node;
     reg_action.info.timer.timeout_msec = timeout_msec;
     reg_action.info.timer.req_type = req_type;
     post_new_reg_action(reg_action);
     return node;
+}
+
+void event_handler_manager::register_socket_timer_event(sockinfo_tcp *sock_tcp)
+{
+    evh_logdbg("Registering TCP socket timer: %p", sock_tcp);
+    reg_action_t reg_action;
+    memset(&reg_action, 0, sizeof(reg_action));
+    reg_action.type = REGISTER_TCP_SOCKET_TIMER;
+    reg_action.info.timer.user_data = sock_tcp;
+    post_new_reg_action(reg_action);
 }
 
 void event_handler_manager::wakeup_timer_event(timer_handler *handler, void *node)
@@ -138,7 +146,6 @@ void event_handler_manager::wakeup_timer_event(timer_handler *handler, void *nod
     reg_action.info.timer.handler = handler;
     reg_action.info.timer.node = node;
     post_new_reg_action(reg_action);
-    return;
 }
 
 void event_handler_manager::unregister_timer_event(timer_handler *handler, void *node)
@@ -172,6 +179,16 @@ void event_handler_manager::unregister_timers_event_and_delete(timer_handler *ha
     memset(&reg_action, 0, sizeof(reg_action));
     reg_action.type = UNREGISTER_TIMERS_AND_DELETE;
     reg_action.info.timer.handler = handler;
+    post_new_reg_action(reg_action);
+}
+
+void event_handler_manager::unregister_socket_timer_and_delete(sockinfo_tcp *sock_tcp)
+{
+    evh_logdbg("Unregistering TCP socket timer: %p", sock_tcp);
+    reg_action_t reg_action;
+    memset(&reg_action, 0, sizeof(reg_action));
+    reg_action.type = UNREGISTER_TCP_SOCKET_TIMER_AND_DELETE;
+    reg_action.info.timer.user_data = sock_tcp;
     post_new_reg_action(reg_action);
 }
 
@@ -425,6 +442,10 @@ void event_handler_manager::update_epfd(int fd, int operation, int events)
 const char *event_handler_manager::reg_action_str(event_action_type_e reg_action_type)
 {
     switch (reg_action_type) {
+    case REGISTER_TCP_SOCKET_TIMER:
+        return "REGISTER_TCP_SOCKET_TIMER";
+    case UNREGISTER_TCP_SOCKET_TIMER_AND_DELETE:
+        return "UNREGISTER_TCP_SOCKET_TIMER_AND_DELETE";
     case REGISTER_TIMER:
         return "REGISTER_TIMER";
     case UNREGISTER_TIMER:
@@ -475,30 +496,18 @@ void event_handler_manager::post_new_reg_action(reg_action_t &reg_action)
 
 void event_handler_manager::priv_register_timer_handler(timer_reg_info_t &info)
 {
-    if (info.group) {
-        info.group->add_new_timer((timer_node_t *)info.node, info.handler, info.user_data);
-    } else {
-        m_timer.add_new_timer(info.timeout_msec, (timer_node_t *)info.node, info.handler,
-                              info.user_data, info.req_type);
-    }
+    m_timer.add_new_timer(info.timeout_msec, (timer_node_t *)info.node, info.handler,
+                          info.user_data, info.req_type);
 }
 
 void event_handler_manager::priv_wakeup_timer_handler(timer_reg_info_t &info)
 {
-    timer_node_t *node = (timer_node_t *)info.node;
-    if (node && !node->group) {
-        m_timer.wakeup_timer(node);
-    }
+    m_timer.wakeup_timer((timer_node_t *)info.node);
 }
 
 void event_handler_manager::priv_unregister_timer_handler(timer_reg_info_t &info)
 {
-    timer_node_t *node = (timer_node_t *)info.node;
-    if (node && node->group) {
-        node->group->remove_timer((timer_node_t *)info.node);
-    } else {
-        m_timer.remove_timer(node, info.handler);
-    }
+    m_timer.remove_timer((timer_node_t *)info.node, info.handler);
 }
 
 void event_handler_manager::priv_unregister_all_handler_timers(timer_reg_info_t &info)
@@ -729,7 +738,17 @@ void event_handler_manager::handle_registration_action(reg_action_t &reg_action)
     }
 
     evh_logfunc("event action %d", reg_action.type);
+    sockinfo_tcp *sock;
     switch (reg_action.type) {
+    case REGISTER_TCP_SOCKET_TIMER:
+        sock = reinterpret_cast<sockinfo_tcp *>(reg_action.info.timer.user_data);
+        sock->get_tcp_timer_collection()->add_new_timer(sock);
+        break;
+    case UNREGISTER_TCP_SOCKET_TIMER_AND_DELETE:
+        sock = reinterpret_cast<sockinfo_tcp *>(reg_action.info.timer.user_data);
+        sock->get_tcp_timer_collection()->remove_timer(sock);
+        delete sock;
+        break;
     case REGISTER_TIMER:
         priv_register_timer_handler(reg_action.info.timer);
         break;

src/core/event/event_handler_manager.h
@@ -42,17 +42,19 @@
 #include "core/infra/subject_observer.h"
 #include "core/event/command.h"
 #include "core/event/delta_timer.h"
-#include "core/event/timers_group.h"
 #include "core/util/xlio_stats.h"
 
 class timer_handler;
 class event_handler_ibverbs;
 class event_handler_rdma_cm;
+class sockinfo_tcp;
 
 typedef std::map<void * /*event_handler_id*/, event_handler_rdma_cm * /*p_event_handler*/>
     event_handler_rdma_cm_map_t;
 
 typedef enum {
+    REGISTER_TCP_SOCKET_TIMER,
+    UNREGISTER_TCP_SOCKET_TIMER_AND_DELETE,
     REGISTER_TIMER,
     WAKEUP_TIMER, /* NOT AVAILABLE FOR GROUPED TIMERS */
     UNREGISTER_TIMER,
@@ -94,7 +96,6 @@ struct timer_reg_info_t {
     void *node;
     unsigned int timeout_msec;
     void *user_data;
-    timers_group *group;
     timer_req_type_t req_type;
 };
 
@@ -158,10 +159,13 @@ public:
     ~event_handler_manager();
 
     void *register_timer_event(int timeout_msec, timer_handler *handler, timer_req_type_t req_type,
-                               void *user_data, timers_group *group = nullptr);
+                               void *user_data);
     void wakeup_timer_event(timer_handler *handler, void *node);
     void unregister_timer_event(timer_handler *handler, void *node);
     void unregister_timers_event_and_delete(timer_handler *handler);
+
+    void register_socket_timer_event(sockinfo_tcp *sock_tcp);
+    void unregister_socket_timer_and_delete(sockinfo_tcp *sock_tcp);
 
     void register_ibverbs_event(int fd, event_handler_ibverbs *handler, void *channel,
                                 void *user_data);

src/core/sock/sockinfo_tcp.cpp
@@ -289,7 +289,6 @@ static inline bool use_socket_ring_locks()
 
 sockinfo_tcp::sockinfo_tcp(int fd, int domain)
     : sockinfo(fd, domain, use_socket_ring_locks())
-    , m_timer_handle(nullptr)
     , m_tcp_con_lock(get_new_tcp_lock())
     , m_sysvar_buffer_batching_mode(safe_mce_sys().buffer_batching_mode)
     , m_sysvar_tx_segs_batch_tcp(safe_mce_sys().tx_segs_batch_tcp)
@@ -597,23 +596,16 @@ void sockinfo_tcp::clean_socket_obj()
     if (is_cleaned()) {
         return;
     }
-
     m_is_cleaned = true;
-    event_handler_manager *p_event_mgr = get_event_mgr();
 
+    unlock_tcp_con();
+
+    event_handler_manager *p_event_mgr = get_event_mgr();
     bool delegated_timers_exit = g_b_exit &&
         (safe_mce_sys().tcp_ctl_thread == option_tcp_ctl_thread::CTL_THREAD_DELEGATE_TCP_TIMERS);
 
-    /* Remove group timers from g_tcp_timers_collection */
-    if (p_event_mgr->is_running() && m_timer_handle && !delegated_timers_exit) {
-        p_event_mgr->unregister_timer_event(this, m_timer_handle);
-    }
-
-    m_timer_handle = nullptr;
-    unlock_tcp_con();
-
     if (p_event_mgr->is_running() && !delegated_timers_exit) {
-        p_event_mgr->unregister_timers_event_and_delete(this);
+        p_event_mgr->unregister_socket_timer_and_delete(this);
     } else {
         delete this;
     }
@@ -1829,9 +1821,8 @@ void sockinfo_tcp::process_rx_ctl_packets()
 }
 
 // Execute TCP timers of this connection
-void sockinfo_tcp::handle_timer_expired(void *user_data)
+void sockinfo_tcp::handle_timer_expired()
 {
-    NOT_IN_USE(user_data);
     si_tcp_logfunc("");
 
     if (tcp_ctl_thread_on(m_sysvar_tcp_ctl_thread)) {
@@ -2509,18 +2500,10 @@ ssize_t sockinfo_tcp::rx(const rx_call_t call_type, iovec *p_iov, ssize_t sz_iov
 
 void sockinfo_tcp::register_timer()
 {
-    if (!m_timer_handle) {
-        si_tcp_logdbg("Registering TCP socket timer: socket: %p, thread-col: %p, global-col: %p",
-                      this, get_tcp_timer_collection(), g_tcp_timers_collection);
+    si_tcp_logdbg("Registering TCP socket timer: socket: %p, thread-col: %p, global-col: %p", this,
+                  get_tcp_timer_collection(), g_tcp_timers_collection);
 
-        /* user_data is the socket itself for a fast cast in the timer_expired(). */
-        m_timer_handle = get_event_mgr()->register_timer_event(
-            safe_mce_sys().tcp_timer_resolution_msec, this, PERIODIC_TIMER,
-            reinterpret_cast<void *>(this), get_tcp_timer_collection());
-    } else {
-        si_tcp_logdbg("register_timer was called more than once. Something might be wrong, or "
-                      "connect was called twice.");
-    }
+    get_event_mgr()->register_socket_timer_event(this);
 }
 
 void sockinfo_tcp::queue_rx_ctl_packet(struct tcp_pcb *pcb, mem_buf_desc_t *p_desc)
@@ -2540,7 +2523,7 @@ void sockinfo_tcp::queue_rx_ctl_packet(struct tcp_pcb *pcb, mem_buf_desc_t *p_de
     }
 
     if (m_sysvar_tcp_ctl_thread == option_tcp_ctl_thread::CTL_THREAD_WITH_WAKEUP) {
-        g_p_event_handler_manager->wakeup_timer_event(this, m_timer_handle);
+        get_tcp_timer_collection()->register_wakeup_event();
     }
 
     return;
@@ -3095,8 +3078,7 @@ int sockinfo_tcp::listen(int backlog)
     BULLSEYE_EXCLUDE_BLOCK_END
 
     if (tcp_ctl_thread_on(m_sysvar_tcp_ctl_thread)) {
-        m_timer_handle = g_p_event_handler_manager->register_timer_event(
-            safe_mce_sys().timer_resolution_msec, this, PERIODIC_TIMER, 0, NULL);
+        g_p_event_handler_manager->register_socket_timer_event(this);
     }
 
     unlock_tcp_con();
@@ -3224,7 +3206,7 @@ int sockinfo_tcp::accept_helper(struct sockaddr *__addr, socklen_t *__addrlen,
 
     if (m_sysvar_tcp_ctl_thread == option_tcp_ctl_thread::CTL_THREAD_WITH_WAKEUP &&
         !m_rx_peer_packets.empty()) {
-        g_p_event_handler_manager->wakeup_timer_event(this, m_timer_handle);
+        get_tcp_timer_collection()->register_wakeup_event();
     }
 
     unlock_tcp_con();
@@ -5939,25 +5921,16 @@ void sockinfo_tcp::put_tcp_seg_cached(struct tcp_seg *seg)
     }
 }
 
-tcp_timers_collection::tcp_timers_collection(int period, int resolution)
+tcp_timers_collection::tcp_timers_collection()
+    : tcp_timers_collection(safe_mce_sys().tcp_timer_resolution_msec /
+                            safe_mce_sys().timer_resolution_msec)
 {
-    m_n_period = period;
-    m_n_resolution = resolution;
-    m_n_intervals_size = period / resolution;
-    m_timer_handle = nullptr;
-    m_p_intervals = new timer_node_t *[m_n_intervals_size];
-    BULLSEYE_EXCLUDE_BLOCK_START
-    if (!m_p_intervals) {
-        __log_dbg("failed to allocate memory");
-        free_tta_resources();
-        throw_xlio_exception("failed to allocate memory");
-    }
+}
 
-    BULLSEYE_EXCLUDE_BLOCK_END
-    memset(m_p_intervals, 0, sizeof(timer_node_t *) * m_n_intervals_size);
-    m_n_location = 0;
-    m_n_next_insert_bucket = 0;
-    m_n_count = 0;
+tcp_timers_collection::tcp_timers_collection(int intervals)
+{
+    m_n_intervals_size = intervals;
+    m_p_intervals.resize(m_n_intervals_size);
 }
 
 tcp_timers_collection::~tcp_timers_collection()
@@ -5979,19 +5952,15 @@ event_handler_manager *tcp_timers_collection::get_event_mgr()
 
 void tcp_timers_collection::free_tta_resources()
 {
-    if (m_n_count) {
-        for (int i = 0; i < m_n_intervals_size; i++) {
-            if (m_p_intervals[i]) {
-                remove_timer(m_p_intervals[i]);
-            }
-        }
-
-        if (m_n_count) {
-            __log_dbg("not all TCP timers have been removed, count=%d", m_n_count);
+    for (auto &bucket : m_p_intervals) {
+        while (!bucket.empty()) {
+            remove_timer(bucket.front());
         }
     }
 
-    delete[] m_p_intervals;
+    if (m_n_count) {
+        __log_dbg("Not all TCP socket timers have been removed, count=%d", m_n_count);
+    }
 }
 
 void tcp_timers_collection::clean_obj()
@@ -6014,14 +5983,10 @@ void tcp_timers_collection::clean_obj()
 void tcp_timers_collection::handle_timer_expired(void *user_data)
 {
     NOT_IN_USE(user_data);
-    timer_node_t *iter = m_p_intervals[m_n_location];
-    sockinfo_tcp *p_sock;
-
+    sock_list &bucket = m_p_intervals[m_n_location];
     m_n_location = (m_n_location + 1) % m_n_intervals_size;
 
-    while (iter) {
-        p_sock = reinterpret_cast<sockinfo_tcp *>(iter->user_data);
-
+    for (sockinfo_tcp *p_sock : bucket) {
         /* It is not guaranteed that the same sockinfo object is met once
          * in this loop.
          * So in case sockinfo object is destroyed other processing
@@ -6031,7 +5996,7 @@ void tcp_timers_collection::handle_timer_expired(void *user_data)
         if (!p_sock->trylock_tcp_con()) {
             bool destroyable = false;
             if (!p_sock->is_cleaned()) {
-                p_sock->handle_timer_expired(iter->user_data);
+                p_sock->handle_timer_expired();
                 destroyable = p_sock->is_destroyable_no_lock();
             }
             p_sock->unlock_tcp_con();
@@ -6039,7 +6004,6 @@ void tcp_timers_collection::handle_timer_expired(void *user_data)
                 g_p_fd_collection->destroy_sockfd(p_sock);
             }
         }
-        iter = iter->next;
     }
 
     /* Processing all messages for the daemon */
@@ -6048,69 +6012,51 @@ void tcp_timers_collection::handle_timer_expired(void *user_data)
     }
 }
 
-void tcp_timers_collection::add_new_timer(timer_node_t *node, timer_handler *handler,
-                                          void *user_data)
+void tcp_timers_collection::add_new_timer(sockinfo_tcp *sock)
 {
-    node->handler = handler;
-    node->user_data = user_data;
-    node->group = this;
-    node->next = nullptr;
-    node->prev = nullptr;
-    if (m_p_intervals[m_n_next_insert_bucket]) {
-        m_p_intervals[m_n_next_insert_bucket]->prev = node;
-        node->next = m_p_intervals[m_n_next_insert_bucket];
-    }
-    m_p_intervals[m_n_next_insert_bucket] = node;
-    m_n_next_insert_bucket = (m_n_next_insert_bucket + 1) % m_n_intervals_size;
-
-    if (m_n_count == 0) {
-        m_timer_handle =
-            get_event_mgr()->register_timer_event(m_n_resolution, this, PERIODIC_TIMER, nullptr);
-    }
-    m_n_count++;
-
-    __log_dbg("new TCP timer handler [%p] was added", handler);
-}
-
-void tcp_timers_collection::remove_timer(timer_node_t *node)
-{
-    if (!node) {
+    if (!sock) {
+        __log_warn("Trying to add timer for null TCP socket %p", sock);
         return;
     }
 
-    node->group = nullptr;
+    sock_list &bucket = m_p_intervals[m_n_next_insert_bucket];
+    bucket.emplace_back(sock);
+    m_sock_remove_map.emplace(sock, std::make_tuple(m_n_next_insert_bucket, --(bucket.end())));
+    m_n_next_insert_bucket = (m_n_next_insert_bucket + 1) % m_n_intervals_size;
 
-    if (node->prev) {
-        node->prev->next = node->next;
-    } else {
-        for (int i = 0; i < m_n_intervals_size; i++) {
-            if (m_p_intervals[i] == node) {
-                m_p_intervals[i] = node->next;
-                break;
+    if (0 == m_n_count++) {
+        m_timer_handle = get_event_mgr()->register_timer_event(safe_mce_sys().timer_resolution_msec,
+                                                               this, PERIODIC_TIMER, nullptr);
+    }
+
+    __log_dbg("New TCP socket [%p] timer was added", sock);
+}
+
+void tcp_timers_collection::remove_timer(sockinfo_tcp *sock)
+{
+    auto node = m_sock_remove_map.find(sock);
+    if (node != m_sock_remove_map.end()) {
+        m_p_intervals[std::get<0>(node->second)].erase(std::get<1>(node->second));
+        m_sock_remove_map.erase(node);
+
+        if (!(--m_n_count)) {
+            if (m_timer_handle) {
+                get_event_mgr()->unregister_timer_event(this, m_timer_handle);
+                m_timer_handle = nullptr;
             }
         }
+
+        __log_dbg("TCP socket [%p] timer was removed", sock);
     }
+}
 
-    if (node->next) {
-        node->next->prev = node->prev;
-    }
-
-    m_n_count--;
-    if (m_n_count == 0) {
-        if (m_timer_handle) {
-            get_event_mgr()->unregister_timer_event(this, m_timer_handle);
-            m_timer_handle = nullptr;
-        }
-    }
-
-    __log_dbg("TCP timer handler [%p] was removed", node->handler);
-
-    free(node);
+void tcp_timers_collection::register_wakeup_event()
+{
+    g_p_event_handler_manager->wakeup_timer_event(this, m_timer_handle);
 }
 
 thread_local_tcp_timers::thread_local_tcp_timers()
-    : tcp_timers_collection(safe_mce_sys().tcp_timer_resolution_msec,
-                            safe_mce_sys().tcp_timer_resolution_msec)
+    : tcp_timers_collection(1)
 {
 }
 

src/core/Makefile.am
@@ -216,7 +216,6 @@ libxlio_la_SOURCES := \
 	event/netlink_event.h \
 	event/poll_group.h \
 	event/timer_handler.h \
-	event/timers_group.h \
 	event/vlogger_timer_handler.h \
 	\
 	ib/base/verbs_extra.h \

src/core/event/delta_timer.h
@@ -39,7 +39,6 @@
 #define INFINITE_TIMEOUT (-1)
 
 class timer_handler;
-class timers_group;
 
 enum timer_req_type_t {
     // reregister itself every after timer expires. (the client doesn't need to reregister)
@@ -65,7 +64,6 @@ struct timer_node_t {
     /* link to the context registered */
     timer_handler *handler;
     void *user_data;
-    timers_group *group;
     timer_req_type_t req_type;
     struct timer_node_t *next;
     struct timer_node_t *prev;

src/core/event/poll_group.cpp
@@ -59,8 +59,7 @@ poll_group::poll_group(const struct xlio_poll_group_attr *attr)
     m_rings.reserve(2);
 
     m_event_handler = std::make_unique<event_handler_manager_local>();
-    m_tcp_timers = std::make_unique<tcp_timers_collection>(
-        safe_mce_sys().tcp_timer_resolution_msec, safe_mce_sys().tcp_timer_resolution_msec);
+    m_tcp_timers = std::make_unique<tcp_timers_collection>(1U);
     m_tcp_timers->set_group(this);
 }
 

src/core/event/timers_group.h
@@ -1,61 +1,0 @@
-/*
- * Copyright (c) 2001-2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
- *
- * This software is available to you under a choice of one of two
- * licenses.  You may choose to be licensed under the terms of the GNU
- * General Public License (GPL) Version 2, available from the file
- * COPYING in the main directory of this source tree, or the
- * BSD license below:
- *
- *     Redistribution and use in source and binary forms, with or
- *     without modification, are permitted provided that the following
- *     conditions are met:
- *
- *      - Redistributions of source code must retain the above
- *        copyright notice, this list of conditions and the following
- *        disclaimer.
- *
- *      - Redistributions in binary form must reproduce the above
- *        copyright notice, this list of conditions and the following
- *        disclaimer in the documentation and/or other materials
- *        provided with the distribution.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
- * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
- * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
-#ifndef TIMERS_GROUP_H
-#define TIMERS_GROUP_H
-
-/*
- * This is an API for batching timers into groups.
- * Instead of registering each timer separately into the internal thread, the group is registered
- * once, and the timers are registered to the group. The registration to the group is still done
- * through the internal thread. The group must be deleted through the internal thread (must
- * implement clean_obj interface). Registering to group must be used with register_timer_event() and
- * unregister_timer_event() only.
- */
-class timers_group : public timer_handler {
-public:
-    virtual ~timers_group() {};
-    // execute all the timers registered to the group
-    // according to the internal group logic.
-    virtual void handle_timer_expired(void *user_data) = 0;
-
-protected:
-    friend class event_handler_manager;
-    // add a new timer
-    virtual void add_new_timer(timer_node_t *node, timer_handler *handler, void *user_data) = 0;
-
-    // remove timer from list and free it.
-    // called for stopping (unregistering) a timer
-    virtual void remove_timer(timer_node_t *node) = 0;
-};
-
-#endif

src/core/main.cpp
@@ -1102,9 +1102,7 @@ static void do_global_ctors_helper()
 
     // For delegated TCP timers the global collection is not used.
     if (safe_mce_sys().tcp_ctl_thread != option_tcp_ctl_thread::CTL_THREAD_DELEGATE_TCP_TIMERS) {
-        NEW_CTOR(g_tcp_timers_collection,
-                 tcp_timers_collection(safe_mce_sys().tcp_timer_resolution_msec,
-                                       safe_mce_sys().timer_resolution_msec));
+        NEW_CTOR(g_tcp_timers_collection, tcp_timers_collection());
     }
 
     NEW_CTOR(g_p_vlogger_timer_handler, vlogger_timer_handler());

commit 864d47db8c1c1c0916b72fbf62d41e302629b4f7

issue: 4409403 Fix heap corruption since c73d96a
This commit fixes a critical race condition in timer management for TCP
sockets that was introduced in commit c73d96a.

The heap corruption was caused by a race condition between the timer
thread and socket destruction. Sockets could be deleted by the event
handler thread while still being referenced by the timer thread in the
timer collections, resulting in heap corruption when the timer thread
attempted to access the deleted memory.

In the original implementation, sockets were removed from timer
collections and deleted asynchronously without proper synchronization
with the timer processing thread.

Fix:
- Remove sockets from timer collections while still holding the socket
  lock, guaranteeing the timer thread cannot access sockets marked for
  deletion
- Create a simplified deletion path that doesn't attempt to access timer
  collections again after socket cleanup

Additionally, as an unrelated improvement, this patch fixes a lock leak
in the early return path of sockinfo_tcp::clean_socket_obj() where a
lock was acquired but not released when a socket was already marked as
cleaned.

The heap corruption stemmed from a fundamental architectural change that
separated socket objects from their timer management without providing
proper synchronization for the distributed socket lifecycle.

Signed-off-by: Tomer Cabouly <tcabouly@nvidia.com>

src/core/event/event_handler_manager.cpp
@@ -156,12 +156,12 @@ void event_handler_manager::unregister_timers_event_and_delete(timer_handler *ha
     post_new_reg_action(reg_action);
 }
 
-void event_handler_manager::unregister_socket_timer_and_delete(sockinfo_tcp *sock_tcp)
+void event_handler_manager::unregister_socket_and_delete(sockinfo_tcp *sock_tcp)
 {
-    evh_logdbg("Unregistering TCP socket timer: %p", sock_tcp);
+    evh_logdbg("Deleting TCP socket: %p", sock_tcp);
     reg_action_t reg_action;
     memset(&reg_action, 0, sizeof(reg_action));
-    reg_action.type = UNREGISTER_TCP_SOCKET_TIMER_AND_DELETE;
+    reg_action.type = UNREGISTER_SOCKET_AND_DELETE;
     reg_action.info.timer.user_data = sock_tcp;
     post_new_reg_action(reg_action);
 }
@@ -421,8 +421,6 @@ const char *event_handler_manager::reg_action_str(event_action_type_e reg_action
     switch (reg_action_type) {
     case REGISTER_TCP_SOCKET_TIMER:
         return "REGISTER_TCP_SOCKET_TIMER";
-    case UNREGISTER_TCP_SOCKET_TIMER_AND_DELETE:
-        return "UNREGISTER_TCP_SOCKET_TIMER_AND_DELETE";
     case REGISTER_TIMER:
         return "REGISTER_TIMER";
     case UNREGISTER_TIMER:
@@ -441,6 +439,8 @@ const char *event_handler_manager::reg_action_str(event_action_type_e reg_action
         return "REGISTER_COMMAND";
     case UNREGISTER_COMMAND:
         return "UNREGISTER_COMMAND";
+    case UNREGISTER_SOCKET_AND_DELETE:
+        return "UNREGISTER_SOCKET_AND_DELETE";
         BULLSEYE_EXCLUDE_BLOCK_START
     default:
         return "UNKNOWN";
@@ -703,9 +703,9 @@ void event_handler_manager::handle_registration_action(reg_action_t &reg_action)
         sock = reinterpret_cast<sockinfo_tcp *>(reg_action.info.timer.user_data);
         sock->get_tcp_timer_collection()->add_new_timer(sock);
         break;
-    case UNREGISTER_TCP_SOCKET_TIMER_AND_DELETE:
+    case UNREGISTER_SOCKET_AND_DELETE:
         sock = reinterpret_cast<sockinfo_tcp *>(reg_action.info.timer.user_data);
-        sock->get_tcp_timer_collection()->remove_timer(sock);
+        // Just delete the socket without trying to remove from timer collection
         delete sock;
         break;
     case REGISTER_TIMER:

src/core/event/event_handler_manager.h
@@ -28,7 +28,7 @@ typedef std::map<void * /*event_handler_id*/, event_handler_rdma_cm * /*p_event_
 
 typedef enum {
     REGISTER_TCP_SOCKET_TIMER,
-    UNREGISTER_TCP_SOCKET_TIMER_AND_DELETE,
+    UNREGISTER_SOCKET_AND_DELETE,
     REGISTER_TIMER,
     WAKEUP_TIMER, /* NOT AVAILABLE FOR GROUPED TIMERS */
     UNREGISTER_TIMER,
@@ -167,7 +167,7 @@ public:
     void unregister_timers_event_and_delete(timer_handler *handler);
 
     void register_socket_timer_event(sockinfo_tcp *sock_tcp);
-    void unregister_socket_timer_and_delete(sockinfo_tcp *sock_tcp);
+    void unregister_socket_and_delete(sockinfo_tcp *sock_tcp);
 
     void register_ibverbs_event(int fd, event_handler_ibverbs *handler, void *channel,
                                 void *user_data);

src/core/sock/sockinfo_tcp.cpp
@@ -594,9 +594,17 @@ void sockinfo_tcp::clean_socket_obj()
     lock_tcp_con();
 
     if (is_cleaned()) {
+        unlock_tcp_con();
         return;
     }
     m_is_cleaned = true;
+
+    // Important: Remove from timer collection WHILE STILL HOLDING THE LOCK
+    // This prevents the timer thread from processing this socket after it's marked for cleanup
+    if (is_timer_registered()) {
+        tcp_timers_collection *p_collection = get_tcp_timer_collection();
+        p_collection->remove_timer(this);
+    }
 
     unlock_tcp_con();
 
@@ -605,7 +613,7 @@ void sockinfo_tcp::clean_socket_obj()
         (safe_mce_sys().tcp_ctl_thread == option_tcp_ctl_thread::CTL_THREAD_DELEGATE_TCP_TIMERS);
 
     if (p_event_mgr->is_running() && !delegated_timers_exit) {
-        p_event_mgr->unregister_socket_timer_and_delete(this);
+        p_event_mgr->unregister_socket_and_delete(this);
     } else {
         delete this;
     }