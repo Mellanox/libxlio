df31d8ce42f4e3ca3b8ae87ec3e4f204e03e1a05
Dmytro Podgornyi <dmytrop@nvidia.com>
2025-08-08 23:50:59 +0300

    issue: 4603449 Add gtests for xlio_socket_destroy()
    xlio_socket_destroy() must trigger XLIO_SOCKET_EVENT_TERMINATED
    regardless of socket state.
    
    Check that a "blank" socket triggers the event on destruction.
    Check that failed connect doesn't trigger the event before destruction.
    
    Signed-off-by: Dmytro Podgornyi <dmytrop@nvidia.com>

--- tests/gtest/xlio_ultra_api/xlio_socket.cc
+++ tests/gtest/xlio_ultra_api/xlio_socket.cc
@@ -10,7 +10,23 @@
 #include "common/base.h"
 #include "core/xlio_base.h"
 
+#include <chrono>
+#include <unordered_map>
+
 #if defined(EXTRA_API_ENABLED) && (EXTRA_API_ENABLED == 1)
+
+class ultra_api_socket_metadata {
+public:
+    bool event_is_triggered(int event) const { return m_events.find(event) != m_events.end(); }
+    void event_trigger(int event, int value) { m_events[event] = value; }
+    int event_triggered_nr() const { return m_events.size(); }
+    int error()
+    {
+        return event_is_triggered(XLIO_SOCKET_EVENT_ERROR) ? m_events[XLIO_SOCKET_EVENT_ERROR] : -1;
+    }
+
+    std::unordered_map<int, int> m_events;
+};
 
 class ultra_api_socket : public ultra_api_base {
 public:
@@ -22,6 +38,18 @@ public:
         UNREFERENCED_PARAMETER(event);
         UNREFERENCED_PARAMETER(value);
     }
+
+    static void socket_event_cb_metadata(xlio_socket_t sock, uintptr_t userdata_sq, int event,
+                                         int value)
+    {
+        UNREFERENCED_PARAMETER(sock);
+
+        ultra_api_socket_metadata *mdata =
+            reinterpret_cast<ultra_api_socket_metadata *>(userdata_sq);
+        ASSERT_TRUE(!mdata->event_is_triggered(event));
+        mdata->event_trigger(event, value);
+    }
+
     static void socket_comp_cb(xlio_socket_t sock, uintptr_t userdata_sq, uintptr_t userdata_op)
     {
         UNREFERENCED_PARAMETER(sock);
@@ -64,13 +92,130 @@ TEST_F(ultra_api_socket, ti_1)
                            &socket_accept_cb);
     xlio_socket_attr sattr = {
         .flags = 0,
-        .domain = AF_INET,
+        .domain = client_addr.addr.sa_family,
         .group = group,
         .userdata_sq = 0,
     };
     base_create_socket(&sattr, &sock);
 
     base_destroy_socket(sock);
+    destroy_poll_group(group);
+}
+
+/**
+ * @test ultra_api_socket.ti_2
+ * @brief
+ *    Test XLIO_SOCKET_EVENT_TERMINATED for a non-connected socket
+ * @details
+ */
+TEST_F(ultra_api_socket, ti_2)
+{
+    xlio_poll_group_t group;
+    xlio_socket_t sock;
+    ultra_api_socket_metadata mdata;
+
+    base_create_poll_group(&group, &socket_event_cb_metadata, &socket_comp_cb, &socket_rx_cb,
+                           &socket_accept_cb);
+    xlio_socket_attr sattr = {
+        .flags = 0,
+        .domain = client_addr.addr.sa_family,
+        .group = group,
+        .userdata_sq = reinterpret_cast<uintptr_t>(&mdata),
+    };
+    base_create_socket(&sattr, &sock);
+
+    base_destroy_socket(sock);
+
+    // Poll group until the TERMINATED event or timeout.
+    auto timeout = std::chrono::microseconds(500);
+    auto start_time = std::chrono::steady_clock::now();
+    bool timedout = false;
+    do {
+        xlio_api->xlio_poll_group_poll(group);
+        timedout = (std::chrono::steady_clock::now() - start_time > timeout);
+    } while (!mdata.event_is_triggered(XLIO_SOCKET_EVENT_TERMINATED) && !timedout);
+
+    ASSERT_TRUE(mdata.event_is_triggered(XLIO_SOCKET_EVENT_TERMINATED));
+    ASSERT_EQ(1, mdata.event_triggered_nr());
+
+    destroy_poll_group(group);
+}
+
+/**
+ * @test ultra_api_socket.ti_3
+ * @brief
+ *    Test XLIO_SOCKET_EVENT_TERMINATED for a socket with failed connect
+ * @details
+ */
+TEST_F(ultra_api_socket, ti_3)
+{
+    xlio_poll_group_t group;
+    xlio_socket_t sock;
+    ultra_api_socket_metadata mdata;
+    int rc;
+
+    sockaddr_store_t fake_addr = server_addr;
+    sys_set_port((struct sockaddr *)&fake_addr, 65535);
+
+    base_create_poll_group(&group, &socket_event_cb_metadata, &socket_comp_cb, &socket_rx_cb,
+                           &socket_accept_cb);
+    xlio_socket_attr sattr = {
+        .flags = 0,
+        .domain = client_addr.addr.sa_family,
+        .group = group,
+        .userdata_sq = reinterpret_cast<uintptr_t>(&mdata),
+    };
+    base_create_socket(&sattr, &sock);
+
+    // Set UTO to fail connect ASAP.
+    unsigned uto_ms = 1U;
+    rc = xlio_api->xlio_socket_setsockopt(sock, IPPROTO_TCP, TCP_USER_TIMEOUT, &uto_ms,
+                                          sizeof(uto_ms));
+    EXPECT_EQ(0, rc);
+
+    rc = xlio_api->xlio_socket_bind(sock, (struct sockaddr *)&client_addr, sizeof(client_addr));
+    ASSERT_EQ(0, rc);
+    rc = xlio_api->xlio_socket_connect(sock, (struct sockaddr *)&fake_addr, sizeof(fake_addr));
+    ASSERT_EQ(0, rc);
+
+    // We expect socket connect to fail.
+    auto timeout = std::chrono::microseconds(1000000);
+    auto start_time = std::chrono::steady_clock::now();
+    bool timedout = false;
+    do {
+        xlio_api->xlio_poll_group_poll(group);
+        timedout = (std::chrono::steady_clock::now() - start_time > timeout);
+    } while (!mdata.event_is_triggered(XLIO_SOCKET_EVENT_ERROR) && !timedout);
+
+    ASSERT_TRUE(mdata.event_is_triggered(XLIO_SOCKET_EVENT_ERROR));
+    ASSERT_EQ(1, mdata.event_triggered_nr());
+    ASSERT_TRUE(mdata.error() == ETIMEDOUT || mdata.error() == ECONNREFUSED)
+        << "Where error is " << mdata.error();
+
+    // TERMINATED event mustn't be triggered before xlio_socket_destroy() is called.
+    timeout = std::chrono::microseconds(500);
+    start_time = std::chrono::steady_clock::now();
+    timedout = false;
+    do {
+        xlio_api->xlio_poll_group_poll(group);
+        timedout = (std::chrono::steady_clock::now() - start_time > timeout);
+    } while (!mdata.event_is_triggered(XLIO_SOCKET_EVENT_TERMINATED) && !timedout);
+
+    ASSERT_TRUE(!mdata.event_is_triggered(XLIO_SOCKET_EVENT_TERMINATED));
+
+    base_destroy_socket(sock);
+
+    // Now we destroy failed socket and the TERMINATED event is expected with the next poll.
+    timeout = std::chrono::microseconds(500);
+    start_time = std::chrono::steady_clock::now();
+    timedout = false;
+    do {
+        xlio_api->xlio_poll_group_poll(group);
+        timedout = (std::chrono::steady_clock::now() - start_time > timeout);
+    } while (!mdata.event_is_triggered(XLIO_SOCKET_EVENT_TERMINATED) && !timedout);
+
+    ASSERT_TRUE(mdata.event_is_triggered(XLIO_SOCKET_EVENT_TERMINATED));
+    ASSERT_EQ(2, mdata.event_triggered_nr());
 
     destroy_poll_group(group);
 }
