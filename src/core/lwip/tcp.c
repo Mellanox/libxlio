50ff91d79d9f46d041f5e2bcc671074c4f7d9505
Dmytro Podgornyi <dmytrop@nvidia.com>
2025-09-10 10:42:05 +0300

    issue: 4315534 Remove TX buffer pre-allocation in LwIP
    Pre-allocation was done only for PBUF_RAM buffers. Also, buffer
    allocation can trigger TX polling in corner cases.
    
    Removing the pre-allocated buffer avoids keeping extra buffer per
    socket. This can save resources in case of large buffers and big number
    of connections.
    
    Signed-off-by: Dmytro Podgornyi <dmytrop@nvidia.com>

--- src/core/lwip/tcp.c
+++ src/core/lwip/tcp.c
@@ -972,7 +972,6 @@ void tcp_pcb_init(struct tcp_pcb *pcb, u8_t prio, void *container)
     pcb->is_in_input = 0;
     pcb->enable_ts_opt = enable_ts_option;
     pcb->seg_alloc = NULL;
-    pcb->pbuf_alloc = NULL;
 }
 
 /**
@@ -1019,10 +1018,6 @@ void tcp_pcb_recycle(struct tcp_pcb *pcb)
         tcp_tx_seg_free(pcb, pcb->seg_alloc);
         pcb->seg_alloc = NULL;
     }
-    if (pcb->pbuf_alloc) {
-        tcp_tx_pbuf_free(pcb, pcb->pbuf_alloc);
-        pcb->pbuf_alloc = NULL;
-    }
 }
 
 struct pbuf *tcp_tx_pbuf_alloc(struct tcp_pcb *pcb, u32_t length, pbuf_type type, pbuf_desc *desc,
@@ -1030,43 +1025,25 @@ struct pbuf *tcp_tx_pbuf_alloc(struct tcp_pcb *pcb, u32_t length, pbuf_type type
 {
     struct pbuf *p;
 
-    if (!pcb->pbuf_alloc || pcb->pbuf_alloc->type != type) {
-
-        // pbuf_alloc is not valid, we should allocate a new pbuf.
-        p = external_tcp_tx_pbuf_alloc(pcb, type, desc, p_buff);
-        if (!p) {
-            return NULL;
-        }
-
-        p->next = NULL;
-        p->type = type;
-        /* set reference count */
-        p->ref = 1;
-        /* set flags */
-        p->flags = 0;
-    } else {
-        // pbuf_alloc is valid, we dont need to allocate a new pbuf element.
-        p = pcb->pbuf_alloc;
-        pcb->pbuf_alloc = NULL;
+    p = external_tcp_tx_pbuf_alloc(pcb, type, desc, p_buff);
+    if (!p) {
+        return NULL;
     }
 
-    /* Set up internal structure of the pbuf. */
+    p->next = NULL;
+    p->type = type;
+    p->ref = 1;
+    p->flags = 0;
     p->len = p->tot_len = length;
 
     return p;
 }
 
-// Release preallocated buffers
 void tcp_tx_preallocted_buffers_free(struct tcp_pcb *pcb)
 {
     if (pcb->seg_alloc) {
         tcp_tx_seg_free(pcb, pcb->seg_alloc);
         pcb->seg_alloc = NULL;
-    }
-
-    if (pcb->pbuf_alloc) {
-        tcp_tx_pbuf_free(pcb, pcb->pbuf_alloc);
-        pcb->pbuf_alloc = NULL;
     }
 }
 
