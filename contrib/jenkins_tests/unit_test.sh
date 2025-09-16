dcfa8ab576b0ba4cd3ac0d7b6311339619aa4d81
NirWolfer <nwolfer@nvidia.com>
2025-09-16 16:06:24 +0300

    [CI] issue: HPCINFRA-3591 Add unit tests to ci
    We would like to run unit tests on CI on one of the OSs to check that
    everything is ok with any changes introduced.
    The unit tests are independent from the build and do not require any
    dependencies such as dpcp or prebuilt xlio lib.
    
    To run the unit-tests we need to just run make command to build them and
    run ./unit_tests from the unit test dir.
    
    Add unit-test.sh script that makes and runs the unit tests
    Add a new Unit tests step in the matrix file
    Rename Test to Socvkperf Test for clarity
    
    Signed-off-by: NirWolfer <nwolfer@nvidia.com>

---
+++ contrib/jenkins_tests/unit_test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -eExo pipefail
+
+UNIT_TEST_DIR=${WORKSPACE}/tests/unit_tests
+JSON_C_DIR=${WORKSPACE}/third_party/json-c
+source $(dirname $0)/globals.sh
+
+# Install dpcp (required for unit tests)
+do_check_env
+do_check_dpcp opt_value
+
+# Prepare libxlio
+./autogen.sh
+./configure --with-dpcp=${opt_value}
+
+# Prepare json-c
+cd ${JSON_C_DIR}
+make "${make_opt}"
+
+# Prepare unit tests
+cd ${UNIT_TEST_DIR}
+make "${make_opt}"
+
+# Run unit tests
+./unit_tests
