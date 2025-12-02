55b4896d0ed02809fbaf218dbd1599e86f00a645
Noam Tsemah <ntsemah@nvidia.com>
2025-12-02 16:01:26 +0200

    [CI] issue: HPCINFRA-4061 Move doca-host installation to image
    Install doca-host during image build instead of runtime to reduce ci duration and avoid network failures.
    
    Signed-off-by: Noam Tsemah <ntsemah@nvidia.com>

--- .ci/scripts/doca_install.sh
+++ .ci/scripts/doca_install.sh
@@ -4,8 +4,9 @@ set -xvEe -o pipefail
 
 DOCA_REPO_PATH="https://doca-repo-prod.nvidia.com/internal/repo/doca"
 TARGET=${TARGET:=all}
-DOCA_VERSION=${DOCA_VERSION:='2.10.0'}
-DOCA_BRANCH=${DOCA_BRANCH:="latest"}
+
+DOCA_VERSION='3.2.0'
+DOCA_BRANCH="latest"
 GPG_KEY="GPG-KEY-Mellanox.pub"
 
 function error_handler() {
@@ -57,7 +58,8 @@ function map_os_and_arch {
             ;;
 
         rhel|ol)
-            OS="${ID}${VERSION_ID}"
+            # Extract major version only (e.g., 9.6 -> 9)
+            OS="${ID}${VERSION_ID%%.*}"
             GPG_KEY_CMD='rpm --import "${GPG_KEY}"'
             REPO_CMD='yum install -y yum-utils && yum-config-manager --add-repo "${REPO_URL}"'
             PKG_TYPE="rpm"
@@ -103,7 +105,7 @@ EOF
 map_os_and_arch
 
 # Install DOCA repo GPG key
-curl -o "${GPG_KEY}" "${DOCA_REPO_PATH}/${DOCA_VERSION}/${OS}/${ARCH}/${DOCA_BRANCH}/${GPG_KEY}" 
+curl -o "${GPG_KEY}" "${DOCA_REPO_PATH}/${DOCA_VERSION}/${OS}/${ARCH}/${DOCA_BRANCH}/${GPG_KEY}"
 
 eval "${GPG_KEY_CMD}"
 
@@ -112,12 +114,17 @@ REPO_URL="${DOCA_REPO_PATH}/${DOCA_VERSION}/${OS}/${ARCH}/${DOCA_BRANCH}/"
 eval "${REPO_CMD}"
 
 # Install DOCA
-${PKG_MGR} ${UPDATE_CMD} 
+${PKG_MGR} ${UPDATE_CMD}
 
-${PKG_MGR} install -y doca-ofed-userspace
+if [[ "$ID" == "ol" && "$ARCH" == "arm64-sbsa" ]]; then
+    ${PKG_MGR} install -y --skip-broken doca-ofed-userspace
+    yumdownloader doca-ofed-userspace && rpm -ivh --nodeps doca-ofed-userspace*.rpm && rm -f doca-ofed-userspace*.rpm
+else
+    ${PKG_MGR} install -y doca-ofed-userspace
+fi
 
 echo "=============================================="
-echo 
+echo
 echo "DOCA for Host has been successfully installed"
 echo
 echo "=============================================="
