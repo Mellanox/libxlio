74ad3aea8954ba5c63f111a0c26ba21d16f65b52
NirWolfer <nwolfer@nvidia.com>
2026-01-07 17:03:41 +0200

    [CI] issue: HPCINFRA-4144 Install private dpcp repo
    Today we are installing libdpcp from the public repository, which might
    not be the latest version available, meaning we might be missing crucial
    bugfixes that are needed to build libxlio
    
    Instead of clonning dpcp at every OS, we can prepare the repo needed in
    advance via cidemo.init script, which will be stashed into the workspace
    for every docker to use.
    This way we only need to compile dpcp from the do_check_dpcp function in
    globals
    
    Clone latest tag available of internal dpcp repo in init script
    Remove clonning and checkout operations from do_check_dpcp function
    
    Signed-off-by: NirWolfer <nwolfer@nvidia.com>

--- contrib/jenkins_tests/unit_test.sh
+++ contrib/jenkins_tests/unit_test.sh
@@ -4,10 +4,12 @@ set -eExo pipefail
 
 UNIT_TEST_DIR=${WORKSPACE}/tests/unit_tests
 JSON_C_DIR=${WORKSPACE}/third_party/json-c
+export jenkins_target="default"
 source $(dirname $0)/globals.sh
 
 # Install dpcp (required for unit tests)
 do_check_env
+
 do_check_dpcp opt_value
 
 # Prepare libxlio
