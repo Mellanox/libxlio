ef6deabdb9ae2507e29ee4d4ed32f28107a86bf1
Dmytro Podgornyi <dmytrop@nvidia.com>
2026-07-27 19:05:12 +0300

    issue: 5165660 Add a regression CI test for json-c build failures
    json-c build system uses AC_CHECK_FUNCS() which can be affected by -Werror
    with some autotools versions (generated C code triggers warnings). Previous
    commit handled this by stripping all the -Werror variants from CFLAGS/CPPFLAGS.
    
    Current commit adds a regression test to catch such situations early.
    
    Signed-off-by: Dmytro Podgornyi <dmytrop@nvidia.com>

--- contrib/jenkins_tests/build.sh
+++ contrib/jenkins_tests/build.sh
@@ -13,15 +13,22 @@ cd ${build_dir}
 # Set symbolic links to default build and install
 ln -s "${build_dir}/0/install" "${install_dir}"
 
+# Regression test against json-c configure-time detection failures: -Werror in
+# CFLAGS used to break the AC_CHECK_FUNCS probes (e.g. the deprecated K&R
+# strdup() check) and misdetect libc functions as missing.
+werror_cflags="-g -Werror -Werror=implicit-function-declaration -Werror-implicit-function-declaration -O2"
+
 declare -A build_list
 build_list['debug']="--enable-opt-log=no --enable-debug"
 build_list['nginx-off']="--enable-nginx=no"
 build_list['envoy-on']="--enable-nginx=yes"
 build_list['static-on']="--enable-static --disable-shared"
+build_list['werror']="CFLAGS=\"${werror_cflags}\""
 build_list['default']=""
 
 build_tap=${WORKSPACE}/${prefix}/build.tap
-echo "1..${#build_list[@]}" > $build_tap
+# +1 for the exported-CFLAGS build below
+echo "1..$((${#build_list[@]} + 1))" > $build_tap
 
 test_id=0
 
@@ -35,6 +42,15 @@ for build_name in "${!build_list[@]}"; do
     test_id=$((test_id+1))
 done
 
+# Same flags, but exported into the environment instead of passed to configure.
+# A global CFLAGS can have a different effect than the configure argument, for
+# example when Makefile.am invokes ./configure (json-c) manually.
+mkdir -p ${build_dir}/${test_id}
+cd ${build_dir}/${test_id}
+test_exec="env CFLAGS=\"${werror_cflags}\" bash -c '${WORKSPACE}/configure --prefix=${build_dir}/${test_id}/install ${jenkins_test_custom_configure} && make ${make_opt} && make install'"
+do_check_result "$test_exec" "$test_id" "werror-env" "$build_tap" "${build_dir}/build-${test_id}"
+cd ${build_dir}
+test_id=$((test_id+1))
 
 echo "[${0##*/}]..................exit code = $rc"
 exit $rc
